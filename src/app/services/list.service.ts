c38411251c10f16310aca35abc93e712b0db3436
Adilson <adilsonczjoaquim@gmail.com>
2022-08-08 15:57:11 +0100

    "Push tudo"

---
+++ src/app/services/list.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Animal } from '../Animal';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ListService {
+  private apiUrl = 'http://localhost:3000/animals'
+
+  constructor(private htttp: HttpClient) { }
+
+  remove(animals: Animal[], animal: Animal){
+    return animals.filter((a) => animal.name !== a.name);
+
+  }
+  getAll(): Observable<Animal[]>{
+    return  this.htttp.get<Animal[]>(this.apiUrl);
+
+  }
+}
